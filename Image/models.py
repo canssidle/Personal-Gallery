67544de837cc456aa60496198f9509cd1bc16040
canssidle <canssidlewairimu@gmail.com>
2019-06-17 11:10:27 +0300

    adding the location class

--- Image/models.py
+++ Image/models.py
@@ -1,14 +1,14 @@
-# from django.db import models
+from django.db import models
 
 
-# import datetime as dt
+import datetime as dt
 
-# # Create your models here.
-# class Location(models.Model):
-#     location = models.CharField(max_length=100)
+# Create your models here.
+class Location(models.Model):
+    location = models.CharField(max_length=100)
 
-#     def __str__(self):
-#         return self.location
+    def __str__(self):
+        return self.location
 
 #     class Meta:
 #         ordering = ['location']
