31fefbec0003834e453b2f13b85274a9fe5b4db9
canssidle <canssidlewairimu@gmail.com>
2019-06-17 11:12:13 +0300

    added methods in the location class

--- Image/models.py
+++ Image/models.py
@@ -10,19 +10,19 @@ class Location(models.Model):
     def __str__(self):
         return self.location
 
-#     class Meta:
-#         ordering = ['location']
+    class Meta:
+        ordering = ['location']
 
-#     def save_location(self):
-#         self.save()
+    def save_location(self):
+        self.save()
 
-#     @classmethod
-#     def delete_location(cls,location):
-#         cls.objects.filter(location=location).delete()
+    @classmethod
+    def delete_location(cls,location):
+        cls.objects.filter(location=location).delete()
 
-#     #    @classmethod
-#     # def delete_category(cls,name):
-#     #     cls.objects.filter(name = name).delete()
+    #    @classmethod
+    # def delete_category(cls,name):
+    #     cls.objects.filter(name = name).delete()
 
 # class categories(models.Model):
 #     category = models.CharField(max_length=30)
