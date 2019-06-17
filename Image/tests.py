8d9513b36327516f8e8ecd4a3d0539ba5a7870e7
canssidle <canssidlewairimu@gmail.com>
2019-06-17 11:16:44 +0300

    test for the categories class

--- Image/tests.py
+++ Image/tests.py
@@ -25,25 +25,25 @@ class LocationTestClass(TestCase):
         locations = Location.objects.all()
         self.assertTrue(len(locations)==0)
 
-# class categoriesTestClass(TestCase):
-#     def setUp(self):
-#         self.Food = categories(category='Food')
+class categoriesTestClass(TestCase):
+    def setUp(self):
+        self.Food = categories(category='Food')
 
-#     def test_instance(self):
-#         self.assertTrue(isinstance(self.Food,categories))
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Food,categories))
 
-#     def tearDown(self):
-#         categories.objects.all().delete()
+    def tearDown(self):
+        categories.objects.all().delete()
 
-#     def test_save_method(self):
-#         self.Food.save_category()
-#         category = categories.objects.all()
-#         self.assertTrue(len(category)>0)
+    def test_save_method(self):
+        self.Food.save_category()
+        category = categories.objects.all()
+        self.assertTrue(len(category)>0)
 
-#     def test_delete_method(self):
-#         self.Food.delete_category('Food')
-#         category = categories.objects.all()
-#         self.assertTrue(len(category)==0)
+    def test_delete_method(self):
+        self.Food.delete_category('Food')
+        category = categories.objects.all()
+        self.assertTrue(len(category)==0)
 
 # class ImageTestClass(TestCase):
 #     def setUp(self):
